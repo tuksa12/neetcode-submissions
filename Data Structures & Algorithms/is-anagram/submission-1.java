bd5334d978e871d1f2250e2897da9c8fbbc2b205
Raphael Tuksa <94465802+tuksa12@users.noreply.github.com>
2026-07-11 16:33:16 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++){
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+                continue;
+            }
+            map.put(s.charAt(i), 1);
+        }
+        System.out.print(map);
+
+        for (int i = 0; i < t.length(); i++){
+            if (map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i),map.get(t.charAt(i))-1);
+                if (map.get(t.charAt(i)) == 0) {
+                    map.remove(t.charAt(i));
+                }
+            }
+        }
+        if (map.keySet().size() == 0){
+            return true;
+        }
+        return false;
+    }
+}
